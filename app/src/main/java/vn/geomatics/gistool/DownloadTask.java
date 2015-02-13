0c780790b577d3693a2d43f23af592f76113b56e
unknown <quan.tran@quantran-pc.niteco.se>
2015-02-13 15:03:29 +0700

    test

---
+++ app/src/main/java/vn/geomatics/gistool/DownloadTask.java
@@ -0,0 +1,12 @@
+package vn.geomatics.gistool;
+
+import android.util.Log;
+
+/**
+ * Created by quan.tran on 2/13/2015.
+ */
+public class DownloadTask {
+    private void doSomething(){
+        Log.d("DKM","Do WTF that you want here");
+    }
+}
